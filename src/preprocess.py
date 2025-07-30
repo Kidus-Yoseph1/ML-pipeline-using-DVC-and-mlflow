f75b8fce7b583ab1823073fc211f566cc350287e
kidus yoseph <kidusy567@gmail.com>
2025-07-30 11:46:35 +0300

    training pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,11 +7,11 @@ import yaml
 
 params = yaml.safe_load(open('params.yaml'))['preprocess']
 
-def preprocess(inputh_path, output_path):
-    data = pd.read_csv(inputh_path)
+def preprocess(input_path, output_path):
+    data = pd.read_csv(input_path)
     
     os.makedirs(os.path.dirname(output_path), exist_ok= True)
-    data.to_csv(output_path, header = None, index= False)
+    data.to_csv(output_path, index= False)
     print(f"preprocessed data saved to {output_path}")
     
     
